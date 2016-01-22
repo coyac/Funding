1247862b4200d91d8e02c7d135c6da04b59a6d3c
coyac <camillecoyac@gmailcom>
2016-01-22 23:39:00 +0000

    First commit

---
+++ client/templates/login/register.js
@@ -0,0 +1,18 @@
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var name = $('[name=name]').val();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        }, function(error){
+            if(error){
+              console.log(error.reason); // Output error if registration fails
+            } else {
+              Router.go("home"); // Redirect user if registration succeeds
+            }
+        }); 
+    }
+});
